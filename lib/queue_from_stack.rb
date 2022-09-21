2fcad024585b0b196ac83d476fcc77d6f368f7ed
Jeff Marks <jeff.t.marks@gmail.com>
2022-09-21 15:10:07 -0400

    done with assignment

--- lib/queue_from_stack.rb
+++ lib/queue_from_stack.rb
@@ -1,3 +1,30 @@
 require_relative './stack'
 
-# your code here
+class MyQueue
+
+	def initialize
+		@s1 = Stack.new
+		@s2 = Stack.new
+	end
+
+	def add(value)
+		@s1.push(value)
+	end
+
+	def remove
+		until @s1.empty? do
+			value = @s1.pop
+			@s2.push(value)
+		end
+		@s2.pop
+	end
+
+	def peek
+		until @s1.empty? do
+			value = @s1.pop
+			@s2.push(value)
+		end
+		@s2.peek
+	end
+
+end
